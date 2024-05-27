0cff55c0f93b3b8b6c991dee1d59dbef3a6e12f0
emmanuel-mutua <emmamulwa2020@gmail.com>
2024-05-27 17:05:45 +0300

    Initial commit

---
+++ src/main/java/org/emmutua/beyondgrain/BeyondGrainApplication.java
@@ -0,0 +1,13 @@
+package org.emmutua.beyondgrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondGrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyondGrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/emmutua/beyondgrain/BeyondGrainApplicationTests.java
@@ -0,0 +1,13 @@
+package org.emmutua.beyondgrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyondGrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
