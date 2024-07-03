9e8709b7005d1d668aa8ee9f4a586fb8daef3a19
emmanuel-mutua <emmamulwa2020@gmail.com>
2024-07-03 16:36:53 +0300

    build docker image

--- src/test/java/org/emmutua/beyondgrain/BeyondGrainApplicationTests.java
+++ src/test/java/org/emmutua/beyondgrain/BeyondGrainApplicationTests.java
@@ -1,13 +1,8 @@
 package org.emmutua.beyondgrain;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BeyondGrainApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
